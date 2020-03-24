f32d2e05b64fda7cf3fce83a410e162cf95ebdaa
Merennage Fernando <DevelopAndTestAndDeploy@gmail.com>
2020-03-24 10:10:23 +0000

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> myClass = Class.forName(Calculator.class.getName());
+            System.out.println(myClass.getName());
+            Constructor<?>[] constructors = myClass.getConstructors();
+            System.out.println(Arrays.toString(constructors));
+            System.out.println(Arrays.toString(myClass.getMethods()));
+
+            Constructor<?> constructor = myClass.getConstructor(null);
+            System.out.println(constructor.newInstance(null));
+
+            Constructor<?> constructor2 = myClass.getConstructor(double.class, double.class);
+            Object myObj = constructor2.newInstance(5, 10);
+
+            Method setNum1 = myClass.getMethod("setNum1", double.class);
+            setNum1.invoke(myObj, 6);
+
+            Method setNum2 = myClass.getMethod("setNum2", double.class);
+            setNum2.invoke(myObj, 8);
+
+            Method method = myClass.getMethod("getNum1", null);
+            System.out.println(method.invoke(myObj, null));
+
+            Method method2 = myClass.getMethod("getNum2", null);
+            System.out.println(method2.invoke(myObj, null));
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
